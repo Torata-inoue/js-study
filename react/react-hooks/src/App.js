96ffa8b5d68bb79a427470ea36ae2b6ad58d7865
Torata-inoue <i_am_torao@yahoo.co.jp>
2021-03-14 13:19:11 +0900

    入門1

--- react/react-hooks/src/App.js
+++ react/react-hooks/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  const time = () => setCount(count * 2);
+  const divide = () => setCount(count => {
+    if (count % 3 === 0) {
+      return count / 3;
+    } else {
+      alert('fuck you!');
+    }
+  });
+
   return (
-    <div>
-      This is Template for react app!
-    </div>
+    <>
+      <div>count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={time}>x2</button>
+      <button onClick={divide}>3の倍数の時だけ３で割る</button>
+    </>
   );
-}
+};
 
 export default App;
