ad54725887776fa1deebf5071007bf8b9d143bb5
Torata-inoue <i_am_torao@yahoo.co.jp>
2021-03-14 16:20:14 +0900

    入門２

--- react/react-hooks/src/App.js
+++ react/react-hooks/src/App.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = props => {
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
-  const time = () => setCount(count * 2);
-  const divide = () => setCount(count => {
-    if (count % 3 === 0) {
-      return count / 3;
-    } else {
-      alert('fuck you!');
-    }
-  });
-
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
   return (
     <>
-      <div>count: {count}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
+      <p>
+        現在の{name}は、{price}です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
       <button onClick={reset}>reset</button>
-      <button onClick={time}>x2</button>
-      <button onClick={divide}>3の倍数の時だけ３で割る</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   );
+};
+
+App.defaultProps = {
+  name: 'sample',
+  price: 1000
 };
 
 export default App;
