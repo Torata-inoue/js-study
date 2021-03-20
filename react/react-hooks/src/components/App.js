4ec1104447f44f4144ccedc12f716c0f13dc5292
Torata-inoue <i_am_torao@yahoo.co.jp>
2021-03-20 18:01:59 +0900

    実践3

--- react/react-hooks/src/components/App.js
+++ react/react-hooks/src/components/App.js
@@ -9,7 +9,10 @@ import AppContext from '../contexts/AppContext.js';
 import reducer from '../reducers';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, []);
+  const initState = {
+    events: []
+  };
+  const [state, dispatch] = useReducer(reducer, initState);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
