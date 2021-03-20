172404d2d5eccfe97640ebaa2356c64d48d7f3eb
Torata-inoue <i_am_torao@yahoo.co.jp>
2021-03-20 11:21:10 +0900

    入門12、13 setup context

--- react/react-hooks/src/components/App.js
+++ react/react-hooks/src/components/App.js
@@ -5,19 +5,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Events from './Events';
 
 import EventForm from './EventForm.js';
-
+import AppContext from '../contexts/AppContext.js';
 import reducer from '../reducers';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <>
+    <AppContext.Provider value={'Hello I am a Provider'}>
       <div className="container-fluid">
         <EventForm state={state} dispatch={dispatch}/>
         <Events state={state} dispatch={dispatch}/>
       </div>
-    </>
+    </AppContext.Provider>
   );
 };
 
