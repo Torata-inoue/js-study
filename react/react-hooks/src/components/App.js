601dc359077788fc331ea7f2aa1335e00835f9a9
Torata-inoue <i_am_torao@yahoo.co.jp>
2021-03-20 19:51:33 +0900

    実践9 ログの表示

--- react/react-hooks/src/components/App.js
+++ react/react-hooks/src/components/App.js
@@ -3,8 +3,8 @@ import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Events from './Events';
-
 import EventForm from './EventForm.js';
+import OperationLogs from './OperationLogs.js';
 import AppContext from '../contexts/AppContext.js';
 import reducer from '../reducers';
 
@@ -20,6 +20,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
