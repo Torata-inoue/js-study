28812e9e95250a9073a2fa41f21a5d5f82d7092c
Torata-inoue <i_am_torao@yahoo.co.jp>
2021-03-20 10:43:06 +0900

    入門11 アクションタイプ定数化

--- react/react-hooks/src/components/Event.js
+++ react/react-hooks/src/components/Event.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { DELETE_EVENT } from "../actions";
+
 const Event = ({ dispatch, event }) => {
   const id = event.id;
   const handleClickDeleteBtn = () => {
     const result = window.confirm(`イベントid=${id}を本当に削除してもいいですか？`);
+
     if (result) {
-      dispatch({ type: 'DELETE_EVENT', id: id })
+      dispatch({ type: DELETE_EVENT, id: id })
     }
   };
 
