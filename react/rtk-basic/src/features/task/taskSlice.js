bedf004328510760e78748e4ab8c1cb6261d7e9a
Torata-Inoue <i_am_torao@yahoo.co.jp>
2021-06-06 15:47:45 +0900

    タスクリスト作成

--- react/rtk-basic/src/features/task/taskSlice.js
+++ react/rtk-basic/src/features/task/taskSlice.js
@@ -1,25 +1,10 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import { fetchCount } from './counterAPI';
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-// export const incrementAsync = createAsyncThunk(
-//   'counter/fetchCount',
-//   async (amount) => {
-//     const response = await fetchCount(amount);
-//     // The value we return becomes the `fulfilled` action payload
-//     return response.data;
-//   }
-// );
+import { createSlice } from '@reduxjs/toolkit';
 
 export const taskSlice = createSlice({
-  name: 'counter',
+  name: 'task',
   initialState: {
     idCount: 3,
-    task: [
+    tasks: [
       {id: 1, title: 'task a', completed: false},
       {id: 2, title: 'task b', completed: true},
       {id: 3, title: 'task c', completed: false}
