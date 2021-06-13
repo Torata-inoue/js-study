1c7b83b3a3f687e61d6fc671bca1fa2ed7efe4f9
Torata-Inoue <i_am_torao@yahoo.co.jp>
2021-06-13 14:16:08 +0900

    function

--- ts/ts-basic/src/index.ts
+++ ts/ts-basic/src/index.ts
@@ -4,8 +4,16 @@
 // const world = new World('Hello TOra!');
 // world.sayHello(root);
 
-import {notExistSample, primitiveSample, anySample, unknownSample} from "./basic";
-primitiveSample();
-notExistSample();
-anySample();
-unknownSample();
+// import {notExistSample, primitiveSample, anySample, unknownSample} from "./basic";
+// primitiveSample();
+// notExistSample();
+// anySample();
+// unknownSample();
+
+import {logMessage} from './function';
+import {isUserSignIn, isUserSignIn2, sumPrice} from "./function/parameters";
+
+logMessage('Hello TOra!');
+isUserSignIn('DFE');
+isUserSignIn2('AAA');
+sumPrice(100, 200, 300, 400);
