e1d7b2638685c519efd158f80bdf77257658f753
Torata-Inoue <i_am_torao@yahoo.co.jp>
2021-06-13 16:24:44 +0900

    taskLIst作成

--- react/rtk_tasks/src/features/login/loginSlice.js
+++ react/rtk_tasks/src/features/login/loginSlice.js
@@ -1,11 +1,11 @@
 import { createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiUrl = 'https://localhost:8000/';
+const apiUrl = 'http://localhost:8000/';
 const token = localStorage.localJWT
 
 export const fetchAsyncLogin = createAsyncThunk('login/post', async (auth) => {
-  const res = await axios.post(`${apiUrl}authen/jwt/create`, auth, {
+  const res = await axios.post(`${apiUrl}authen/jwt/create/`, auth, {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -14,7 +14,7 @@ export const fetchAsyncLogin = createAsyncThunk('login/post', async (auth) => {
 })
 
 export const fetchAsyncRegister = createAsyncThunk('login/register', async (auth) => {
-  const res = await axios.post(`${apiUrl}api/register`, auth, {
+  const res = await axios.post(`${apiUrl}api/register/`, auth, {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -68,7 +68,7 @@ const loginSlice = createSlice({
 
 export const { editUserName, editPassword, toggleMode } = loginSlice.actions;
 export const selectAuthen = state => state.login.authen;
-export const selectIsLogin = state => state.login.isLoginView;
+export const selectIsLoginView = state => state.login.isLoginView;
 export const selectProfile = state => state.login.profile;
 
 export default loginSlice.reducer;
